9b2a97a8093998b9bede1d0fef4688fa5abd58ec
Andreas H <andi@blafasl.de>
2024-12-08 16:00:10 +0000

    fix mkimage.sh for different workspace names

--- mkimage.sh
+++ mkimage.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+outdir=$(find /workspaces/ -name iso -type d)
+
 if [ -z "$1" ]; then
 	prof=n01dart
 else
@@ -8,7 +10,7 @@ fi
 
 cd ~
 sh aports/scripts/mkimage.sh --tag edge \
-	--outdir /workspaces/n01dart/iso \
+	--outdir $outdir \
 	--arch x86_64 \
 	--repository https://dl-cdn.alpinelinux.org/alpine/edge/main \
 	--repository https://dl-cdn.alpinelinux.org/alpine/edge/community \
